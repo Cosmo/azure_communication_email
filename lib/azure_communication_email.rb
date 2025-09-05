136e960f5d355623a33669d5cbbf8baae0312eef
Devran Cosmo Uenal <maccosmo@gmail.com>
2025-09-06 00:38:22 +0200

    Initial commit

---
+++ lib/azure_communication_email.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "azure_communication_email/version"
+
+module AzureCommunicationEmail
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "azure_communication_email"
+
+require "minitest/autorun"
